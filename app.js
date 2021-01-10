42b6d62503f6714a95b752022e2026ace8738651
Katharina Wolf <wolfkatharina89@gmail.com>
2021-01-10 18:58:47 +0100

    Bootstrap fixings

--- app.js
+++ app.js
@@ -1,49 +1,90 @@
 const express = require("express");
+
 const https = require("https");
 const bodyParser = require("body-parser");
-require('dotenv').config()
+
+// const handlebars = require("express-handlebars");
 const app = express();
+// //Sets our app to use the handlebars engine
+// app.set("view engine", "handlebars");
 
+// //Sets handlebars configurations (we will go through them later on)
+// app.engine(
+//     "handlebars",
+//     handlebars({
+//         layoutsDir: __dirname + "/views/layouts",
+//     })
+// );
 
-app.use(bodyParser.urlencoded({extended:true}));
+require("dotenv").config();
+
+const router = express.Router();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res){
-    
+app.post("/", function (req, res) {
     const userSearch = req.body.city;
     const apiKey = process.env.API_KEY;
-    const url = "https://api.openweathermap.org/data/2.5/weather?appid=" + apiKey + "&q=" +userSearch+ "&units=metric";
+    const url =
+        "https://api.openweathermap.org/data/2.5/weather?appid=" +
+        apiKey +
+        "&q=" +
+        userSearch +
+        "&units=metric";
 
-    
-    https.get(url, function(response){
+    https.get(url, function (response) {
         // console.log(response.statusCode);
-        response.on("data", function(data){
-  
-    //parsing data
-        const weatherData = JSON.parse(data);
-    // console.log("response.status:" , weatherData.cod);
-        if (weatherData.cod === 200){
-           
-        const temp = Math.round(weatherData.main.temp);
-        const description = weatherData.weather[0].description;
-        const icon = "http://openweathermap.org/img/wn/" +weatherData.weather[0].icon+ "@2x.png";
-        
-        res.write("<p>The weather in " + weatherData.name + " is currently " + description + ".</p>");
-        res.write("<h1>The temperature in " + weatherData.name + " is " + temp+ " degrees C.</h1>");
-        res.write("<img src=" +icon+ ">");
-        
+        response.on("data", function (data) {
+            //parsing data
+            const weatherData = JSON.parse(data);
+            // console.log("response.status:" , weatherData.cod);
+            if (weatherData.cod === 200) {
+                const temp = Math.round(weatherData.main.temp);
+                const description = weatherData.weather[0].description;
+                const icon =
+                    "http://openweathermap.org/img/wn/" +
+                    weatherData.weather[0].icon +
+                    "@2x.png";
 
-        }else{
+                const image = "<img src=" + icon + ">";
 
-            res.write("<h1>OH NO! Something went wrong... Try again!</h1>")
-        }
-            res.send();
-    })
-    })        
-})
+                res.write(
+                    "<p>The weather in " +
+                        weatherData.name +
+                        " is currently " +
+                        description +
+                        ".</p>"
+                );
+                res.write(
+                    "<h1>The temperature in " +
+                        weatherData.name +
+                        " is " +
+                        temp +
+                        " degrees C.</h1>"
+                );
+                res.write("<img src=" + icon + ">");
+
+                // var name = "hello";
+                // res.render(__dirname + "/weatherResult.html", {
+                //     name: name,
+                // });
+                // res.sendFile(__dirname + "/weatherResult.html");
+                res.send();
+            } else {
+                res.sendFile(__dirname + "/failure.html");
+            }
+        });
+    });
+});
+
+app.post("/failure", function (req, res) {
+    res.redirect("/");
+});
 
 app.listen(8080, function () {
     console.log("Listening on Port 8080");
